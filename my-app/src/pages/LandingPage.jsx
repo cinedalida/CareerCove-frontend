286e61235b94cfe6bc2a3df2bcb48b1f2c3666b3
Francine Dalida <francine.dalida@gmail.com>
2025-10-04 12:30:47 +0800

    Initial Commit
    October 4, 2025

---
+++ my-app/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "../styles/LandingPage.css";
+
+function LandingPage() {
+  return (
+    <>
+      <h1> CareerCove landing page</h1>
+    </>
+  );
+}
+
+export default LandingPage;
