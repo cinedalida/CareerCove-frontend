a6c7b99f55b424470536e880a9e6c0a16bbf9b78
Francine Dalida <francine.dalida@gmail.com>
2025-10-31 12:08:08 +0800

    Added: the modified files

--- my-app/src/pages/LandingPage.jsx
+++ my-app/src/pages/LandingPage.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
 import "../styles/LandingPage.css";
+import Navbar from "../components/PageLayout/Navbar";
+import Footer from "../components/PageLayout/Footer";
+import Hero from "../components/LandingPage/Hero";
+import HowItWorks from "../components/LandingPage/HowItWorks";
+import InfiniteMovingCardsDemo from "../components/LandingPage/infinite-moving-cards-demo";
+import Features from "../components/LandingPage/Features";
+// import LoginPage from "../pages/LoginPage";
 
-function LandingPage() {
+const LandingPage = () => {
   return (
     <>
-      <h1> CareerCove landing page</h1>
+      <Navbar />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      {/* <Feedbacks /> */}
+      <InfiniteMovingCardsDemo />
+      {/* <LoginPage /> */}
+      <Footer />
     </>
   );
-}
+};
 
 export default LandingPage;
