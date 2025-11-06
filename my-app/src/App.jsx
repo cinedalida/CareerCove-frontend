3528baf86f656e595799e9f45c6ce513ae5a2c2c
Francine Dalida <francine.dalida@gmail.com>
2025-11-07 00:25:15 +0800

    Added: routes

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -3,6 +3,9 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Guest from "./pages/Guest";
+import MyProfile from "./pages/MyProfile";
+import Jobs from "./pages/Jobs";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/guest" element={<Guest />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+        <Route path="/jobs" element={<Jobs />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
