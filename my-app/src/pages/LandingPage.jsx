ad16743dfcc0a512e99d4657b76acba0a66688b3
Francine Dalida <francine.dalida@gmail.com>
2025-11-09 18:19:14 +0800

    Added: My Profile componentes 'blank'

--- my-app/src/pages/LandingPage.jsx
+++ my-app/src/pages/LandingPage.jsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import "../styles/LandingPage.css";
 import Navbar from "../components/PageLayout/Navbar";
 import Footer from "../components/PageLayout/Footer";
 import Hero from "../components/LandingPage/Hero";
 import HowItWorks from "../components/LandingPage/HowItWorks";
 import InfiniteMovingCardsDemo from "../components/LandingPage/infinite-moving-cards-demo";
 import Features from "../components/LandingPage/Features";
-// import LoginPage from "../pages/LoginPage";
 
 const LandingPage = () => {
   return (
